4065781d502c19de94b2caa67fdad677b6baefd7
Austi Gage <augage1031@gmail.com>
2023-11-20 18:34:15 -0500

    Header component and other small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './common/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bwm-project';
+  title = 'App component title';
+
+  clickHandler() {
+    alert("HELLO THERE!")
+  }
 }
